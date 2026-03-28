4ebd46d7dd7eb3265d5f26569d02fc69881a1205
Shrutidg617 <gshrutid@gmail.com>
2026-03-28 11:13:22 +0530

    Fix: src package init, preprocess import, procfile port

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,11 +1,8 @@
 import pandas as pd
-
-from feature_engineering import create_features
+from src.feature_engineering import create_features
 
 def load_and_process(path):
     df = pd.read_csv(path, parse_dates=['Datetime'])
     df = df.sort_values('Datetime')
-
     df = create_features(df)
-
     return df
